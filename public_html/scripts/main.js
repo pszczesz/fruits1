e555ee7ab4dd9134254cfcd96dde7743cbb963b1
pszczesz <pszczesz@gmail.com>
2016-04-01 13:48:02 +0200

    new

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,30 +1,67 @@
 
 
 $(document).ready(function () {
+    $("input#start").click(function(){
+        loadOneImage($("div#scene"));
+        loadOnlyOneImage(wzorzec);
+         moveElementInScene();
+    });
     var wzorzec = $("div#wzorzec");
     var upBorder = $("#upBorder");
-    loadOneImage(wzorzec);
+    
+    
     loadAllImages(upBorder);
+   
+    $("input#stop").click(function (){
+        moveElementStop();
+    });
 });
 function getImages() {
     return ['1.png', '2.png', '3.png', '4.png', '5.png'];
 }
 
-function loadOneImage(element){
+function loadOneImage(element) {
     var images = getImages();
-    var los = Math.floor(Math.random()*images.length);
+    var los = Math.floor(Math.random() * images.length);
     console.log(los);
-    var ob = "<img src='images/figury/"+images[los]
-        +"' alt='obrazek'/>";
+    var ob = "<img src='images/figury/" + images[los]
+            + "' alt='obrazek'/>";
 
-      element.html(ob);
+    element.html(ob+element.html());
 }
-function loadAllImages(element){
+function loadOnlyOneImage(element){
+    var images = getImages();
+    var los = Math.floor(Math.random() * images.length);
+    console.log(los);
+    var ob = "<img src='images/figury/" + images[los]
+            + "' alt='obrazek'/>";
+
+    element.html(ob);
+}
+function loadAllImages(element) {
     var images = getImages();
     var html = "";
-    images.forEach(function(item){
+    images.forEach(function (item) {
         html += "<span class='cell'><img src='images/figury/"
-                +item+"' alt=''/></span>";
+                + item + "' alt=''/></span>";
     });
     element.html(html);
+}
+function moveElementInScene() {
+    var element = $("div#scene img");
+    // alert(element.prop("src"));
+    // element.prop("position","absolute");
+    element.css({position: "absolute", left: "100px","margin-top":"45px"});
+    element.load(function () {
+        element.stop();
+      //  console.log("ll = " + element.width());
+        var max = $("div#scene").width() - 2 * element.width();
+       // console.log(max);
+        element.animate({left: max}, 5000);
+    });
+
+}
+function moveElementStop(){
+   var element = $("div#scene img");
+   element.stop();
 }
