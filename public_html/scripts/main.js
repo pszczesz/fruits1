a297091b4ff7b8b72adc271fd6fc4335b803d149
pszczesz <pszczesz@poczta.onet.pl>
2016-04-01 08:47:22 +0200

    ini

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+$(document).ready(function () {
+    var wzorzec = $("div#wzorzec");
+    var upBorder = $("#upBorder");
+    loadOneImage(wzorzec);
+    loadAllImages(upBorder);
+});
+function getImages() {
+    return ['1.png', '2.png', '3.png', '4.png', '5.png'];
+}
+
+function loadOneImage(element){
+    var images = getImages();
+    var los = Math.floor(Math.random()*images.length);
+    console.log(los);
+    var ob = "<img src='images/figury/"+images[los]
+        +"' alt='obrazek'/>";
+
+      element.html(ob);
+}
+function loadAllImages(element){
+    var images = getImages();
+    var html = "";
+    images.forEach(function(item){
+        html += "<span class='cell'><img src='images/figury/"
+                +item+"' alt=''/></span>";
+    });
+    element.html(html);
+}
